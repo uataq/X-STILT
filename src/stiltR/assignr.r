bd0edf677391e6a632d46a879a3838a8a8107f05
wde0924 <wde0924@gmail.com>
2018-05-02 23:53:01 -0600

    first commit

--- src/stiltR/assignr.r
+++ src/stiltR/assignr.r
@@ -1,4 +1,5 @@
-assignr<-function(xname, value, path="", printTF=FALSE,gz=F){
+assignr <- function(xname, value, path="", printTF=FALSE, gz=F){
+
 #see also getr() and existsr()
 #to avoid large databases that take too long to load memory (as R tries...)
 #similar to assign(), but object is removed after call to assignr
@@ -9,16 +10,27 @@ assignr<-function(xname, value, path="", printTF=FALSE,gz=F){
 #printTF can be set to TRUE to print a statement that the object has been assigned
 #2/27/04 by CHG
 #modified to allow compression of files (flag gz)
-#edited by DW on 01/16/2017 (change .RDataxname to xname.RData, no more hidden files)
+
+
+# change .RDataxname to xname.RData, no more hidden files, DW on 01/16/2017
+# change paste() to file.path(), DW, 05/02/2018
 
 #  $Id: assignr.r,v 1.4 2008-08-12 08:51:22 skoerner Exp $
 #---------------------------------------------------------------------------------------------------
-     assign(x=xname,value=value,pos=1)
-     save(list=xname,file=paste(path,xname,".RData",sep=""))
-     remove(list=xname,pos=1)
+
+     assign(x=xname, value=value, pos=1)
+
+     # change paste() to file.path(), DW, 05/02/2018
+     rfile <- file.path(path, xname)
+     #save(list=xname, file=paste(path,xname,".RData",sep=""))
+     save(list=xname, file=rfile)
+     remove(list=xname, pos=1)
+
      if(gz){
-       unix(paste("gzip ",path,xname,".RData",sep=""))
+       unix(paste("gzip ",rfile,".RData",sep=""))
        xname<-paste(xname,".gz",sep="")
      }
-     if (printTF) cat(path, xname, ".RData created\n", sep="")
+     
+     if (printTF) cat(rfile, ".RData created\n", sep="")
+
 }
--- src/stiltR/getr.r
+++ src/stiltR/getr.r
@@ -1,4 +1,5 @@
-getr<-function(xname, path="./",gz=FALSE){
+getr <- function(xname, path="./", gz=FALSE){
+
 #see also assignr() and existsr()
 #to avoid large databases that take too long to load memory (as R tries...)
 #similar to get()
@@ -6,28 +7,38 @@ getr<-function(xname, path="./",gz=FALSE){
 #name of object is xname if file was writen with assignr()
 #2/27/04 by CHG
 #modified to gunzip gzipped files (flag gz)
-#edited by DW on 01/16/2017 (change .RDataxname to xname.RData, no more hidden files)
+
+# change .RDataxname to xname.RData, no more hidden files, DW on 01/16/2017
+# change paste() to file.path(), DW, 05/02/2018
 
 #  $Id: getr.r,v 1.5 2008-03-26 19:19:17 tnehrkor Exp $
 #---------------------------------------------------------------------------------------------------
 
-     xxname<-paste(xname,".RData",sep="")
+     xxname <- paste(xname, ".RData", sep="")
+
+     # change paste() to file.path(), DW, 05/02/2018
+     rfile <- file.path(path, xxname)
+
      if(gz){
-       fls<-dir(path,pattern=xxname,all.files=TRUE)
-       if(xxname%in%fls) {
+       fls <- dir(path, pattern=xxname, all.files=TRUE)
+       if(xxname %in% fls){
          print("found unzipped version, use this")
-         delflag<-F
+         delflag <- F
        }else{
-         unix(paste("gunzip -c ",path,xname,".RData.gz > ",path,xname,".RData",sep=""))
-         delflag<-T
-       }
-     }
-     attach(paste(path,xname,".RData",sep=""),pos=2)
-     dat<-get(xname,pos=2)
+         # unzip gz file
+         unix(paste("gunzip -c ", rfile, ".gz > ", rfile, sep=""))
+         delflag <- T
+       }  # end if find file
+     }  # end if gz
+
+     #attach(paste(path,xname,".RData",sep=""),pos=2)
+     attach(rfile, pos=2)
+     dat <- get(xname, pos=2)
      detach(2)
+
      if(gz){
-       if(delflag){#delete unzipped file, wasn't there before, avoid build up ...
-         unix(paste("rm ",path,xname,".RData",sep=""))
+       if(delflag){ #delete unzipped file, wasn't there before, avoid build up ...
+         unix(paste("rm ", rfile, sep=""))
        }
      }
      return(dat)
