80d97b47227d17fe8f7b368765e9a92c578002d5
wde0924 <wde0924@gmail.com>
2018-05-10 11:46:49 -0600

    'delete previous control and bash files'

--- src/xstiltR/allocate.recp.r
+++ src/xstiltR/allocate.recp.r
@@ -2,14 +2,30 @@
 # write into control file for X-STILT
 # DW, 05/09/2018
 
-allocate.recp <- function(namelist, run.name="run_XSTILT"){
+allocate.recp <- function(namelist){
 
-  # store namelist to output directory
-  filenm <- paste("trajlist_", namelist$site, "_", namelist$timestr, sep="")
-  if(namelist$forwardTF == F)filenm <- paste(filenm, "_backward.txt", sep="")
-  if(namelist$forwardTF)filenm <- paste(filenm, "_forward.txt", sep="")
+  ## before writing, delete previous run_XSTILT and each trajlist* and CONTROL*
+  if(length(list.files(namelist$workdir, "run_XSTILT"))>0){
+    file.remove(list.files(namelist$workdir, "run_XSTILT"))
+  }
 
-  filenm <- file.path(namelist$workdir, filenm)  # link to path
+  if(length(list.files(namelist$workdir, "XSTILT_CONTROL"))>0){
+    file.remove(list.files(namelist$workdir, "XSTILT_CONTROL"))
+  }
+
+  if(length(list.files(namelist$workdir, "trajlist_"))>0){
+    file.remove(list.files(namelist$workdir, "trajlist_"))
+  }
+
+  # create new names
+  namelist$RUN <- paste("run_XSTILT_", namelist$timestr, sep="")
+  namelist$CONTL <- paste("XSTILT_CONTROL_", namelist$timestr, "_copy", sep="")
+
+  # store namelist
+  str <- "backward"; if(namelist$forwardTF)str <- "forward"
+  namelist$TRAJLIST <- paste("trajlist_", namelist$site, "_", namelist$timestr,
+                       "_", str, sep="")
+  trajlist <- file.path(namelist$workdir, namelist$TRAJLIST)  # link to path
 
   # how many copies?
   ncp <- length(namelist$nummodel)
@@ -25,15 +41,14 @@ allocate.recp <- function(namelist, run.name="run_XSTILT"){
      # write CONTROL files for X-STILT, not STILT
      nrecp <- length(recp.list[[1]])
      sep.recp <- round(nrecp/ncp)
-     group.index <- seq(1, nrecp, sep.recp)
+     group.index <- c(seq(1, nrecp, sep.recp), nrecp+1)
 
      for(n in 1:ncp){
-
-       #cat(paste("working on allocating copy #", all.copy[n],"...\n"))
 
        list.index <- group.index[n]:(group.index[n+1]-1)
        group.namelist <- sapply(recp.list, "[", list.index)
 
+       # merge selected receptor info back to common.list
        merge.list <- common.list
        merge.list$recp.lat <- group.namelist[, "recp.lat"]
        merge.list$recp.lon <- group.namelist[, "recp.lon"]
@@ -44,7 +59,7 @@ allocate.recp <- function(namelist, run.name="run_XSTILT"){
        merge.list$nummodel <- all.copy[n]  # select copy number
 
        # output namelist into rds file
-       group.filenm <- gsub(".txt", paste("_cp", n, ".rds", sep=""), filenm)
+       group.filenm <- paste(trajlist, "_cp", n, ".rds", sep="")
        saveRDS(object=merge.list, file=group.filenm)
 
        # write startup r code as well
@@ -55,16 +70,14 @@ allocate.recp <- function(namelist, run.name="run_XSTILT"){
                      "namelist=readRDS(filenm)\n",
                      "run.backward.trajec(namelist=namelist)", sep="")
        cont.filenm <- file.path(namelist$workdir,
-                                paste("XSTILT_CONTROL_copy", all.copy[n],
-                                      ".r", sep=""))
+                              paste(namelist$CONTL, all.copy[n], ".r", sep=""))
        write(cont, cont.filenm)
-       #cat(paste("allocate.recp(): Done writing X-STILT start-up r code ...\n"))
      } # end for n
 
   }else{
 
     # if using onely 1 copy
-    one.filenm <- gsub(".txt", paste("_cp", n, ".rds", sep=""), filenm)
+    one.filenm <- paste(trajlist, "_cp", n, ".rds", sep="")
     saveRDS(object=namelist, file=one.filenm)
 
     # write startup r code as well
@@ -74,43 +87,50 @@ allocate.recp <- function(namelist, run.name="run_XSTILT"){
                   "source(file.path(workdir, 'src', 'sourceall.r'))\n",
                   "namelist=readRDS(filenm)\n",
                   "run.backward.trajec(namelist=namelist)", sep="")
-    cont.filenm <- file.path(namelist$workdir, paste("XSTILT_CONTROL_copy",
-                                                    all.copy, ".r", sep=""))
+    cont.filenm <- file.path(namelist$workdir, paste(namelist$CONTL, all.copy,
+                                                     ".r", sep=""))
     write(cont, cont.filenm)
-    #cat(paste("allocate.recp(): Done writing X-STILT start-up r code ...\n"))
-  }  # end if
+  }  # end if one or multiple nodes
+
   cat(paste("allocate.recp(): Done with X-STILT start-up r code ...\n"))
 
-  #### next, write the parallel txt file as well
-  parfile <- file.path(namelist$workdir, run.name)
+  #### next, write the parallel run file as well
+  parfile <- file.path(namelist$workdir, namelist$RUN)
 
   write("#!/bin/bash\n", parfile)
   write('echo "user is $USER"\n', parfile, append=T)
 
+  # write lines for loading CHPC modules
   mods <- c("R/3.4.3", "ncl", "netcdf-f/4.4.4", "pgi/17.4", "mvapich2/2.2.p",
              "gcc/4.8.5")
   module <- paste("module load", mods)
   write(module, file=parfile, sep="\n", append=T)
 
+  # go to working directory
   write(paste('\ncd ', namelist$workdir, '\n', sep=""), parfile, append=T)
 
   # point to different CONTROL R scripts
   nodes <- seq(0, ncp-1, 1)
-  export.r <- paste("export script_", nodes, "=XSTILT_CONTROL_copy",
-                    all.copy, ".r", sep="")
-  write(export.r, parfile, append=T)
+  r.scripts <- paste("export script_", nodes, "=",
+                     namelist$CONTL, all.copy, ".r", sep="")
+  write(r.scripts, parfile, append=T)
 
-  # excutable codes
-  #Rscript $(eval echo \$script_${1})
-
+  # print some info
   echo <- paste('\necho "Calculation (Core:$1) started at `date`"\necho $1',
                 '\necho "You are in `pwd`"\n', sep="")
-
   write(echo, parfile, append=T)
 
+  # excutable R codes
   exec <- paste('\nRscript $(eval echo \\$script_${1})\n',
                 'echo "Calculation (Core:$1 ) stopped at `date`"\n', sep="")
   write(exec, parfile, append=T)
- cat(paste("allocate.recp(): Done with run_X-STILT...\n"))
+  cat(paste("allocate.recp(): Done with run_X-STILT...\n"))
 
+
+  # finally, turn on permission of all files
+  system(paste("chmod u+x ", parfile, sep=""))
+  cont.files <- list.files(namelist$workdir, "XSTILT_CONTROL")
+  Sys.chmod(cont.files, mode = "744")  # x for me
+
+  return(namelist)
 }  # end subroutine
--- src/xstiltR/write.bash.r
+++ src/xstiltR/write.bash.r
@@ -2,12 +2,21 @@
 # to run this .sh file, "sbatch *.sh"
 # by DW, 05/09/2018
 
-write.bash <- function(job.time="24:00:00", num.nodes, num.cores,
+write.bash <- function(namelist, job.time="24:00:00", num.nodes, num.cores,
                        account="lin-kp", partition="lin-kp",
-                       email="dien.wu@utah.edu", email.type=c("FAIL,BEGIN,END"),
-                       workdir, filenm="XSTILT.sh", run.filenm="run_XSTILT"){
+                       email="dien.wu@utah.edu",
+                       email.type=c("FAIL,BEGIN,END")){
 
-  write("#!/bin/bash", filenm)
+   # delete previous one
+   if(length(list.files(namelist$workdir, ".sh"))>0){
+    file.remove(list.files(namelist$workdir, ".sh"))
+   }
+
+  namelist$BASH <- paste("XSTILT_", namelist$timestr, ".sh", sep="")
+  bash.file <- file.path(namelist$workdir, namelist$BASH) # store at workdir
+  run.file <- file.path(namelist$workdir, namelist$RUN)
+
+  write("#!/bin/bash", bash.file)
 
   sbatch.setup <- paste("\n#SBATCH --time=", job.time,
                         "\n#SBATCH --nodes=", num.nodes,
@@ -17,19 +26,21 @@ write.bash <- function(job.time="24:00:00", num.nodes, num.cores,
                         "\n#SBATCH --mail-user=", email,
                         "\n#SBATCH --mail-type=", email.type,"\n", sep="")
 
-  write(sbatch.setup, file=filenm, append=T)
+  write(sbatch.setup, file=bash.file, append=T)
 
-  write("rm -f multiprog.conf\n", filenm, append=T)
+  write("rm -f multiprog.conf\n", bash.file, append=T)
 
-  # loop over each job
+  # loop over each job and output in configuration file
   job.loop <- paste("JOBNR=0\nSLURM_NTASKS=", num.cores,
                     "\nwhile [[ $JOBNR -lt $SLURM_NTASKS ]]; do\n",
-                    "  echo $JOBNR ", file.path(workdir, run.filenm),
+                    "  echo $JOBNR ", run.file,
                     " $JOBNR >> multiprog.conf\n",
                     "  let JOBNR=JOBNR+1\ndone\n", sep="")
+  write(job.loop, bash.file, append=T)
 
-  write(job.loop, filenm, append=T)
-
-  write("srun --multi-prog multiprog.conf\n", filenm, append=T)
+  # submit job
+  write("srun --multi-prog multiprog.conf\n", bash.file, append=T)
   cat(paste("write.bash(): Done with bash file...\n"))
+
+  return(bash.file)
 }
